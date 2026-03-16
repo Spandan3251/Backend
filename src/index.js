5ac4f373c65176b30ddb02974355c7e2b97ddac9
Spandan Polley <polleyspandan3251@gmail.com>
2026-03-16 20:08:20 +0530

    utils discussion

--- src/index.js
+++ src/index.js
@@ -9,6 +9,18 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+            console.log("Error:",error);
+            throw error
+        })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection FAILED !!",error)
+})
 
 
 /*
