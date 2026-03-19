d0bd0d2b7a3af95ab994a5aba2cd4c24535078d4
Spandan Polley <polleyspandan3251@gmail.com>
2026-03-19 22:18:49 +0530

    all setup completed

--- src/app.js
+++ src/app.js
@@ -9,9 +9,20 @@ app.use(cors({
     credentials:true
 }))
 
+
+
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from './routes/user.routes.js'
+
+
+//routes declaration
+app.use("/api/v1/user",userRouter)
+
+// http://localhost:8000/api/v1/user/register
 
 export {app}
